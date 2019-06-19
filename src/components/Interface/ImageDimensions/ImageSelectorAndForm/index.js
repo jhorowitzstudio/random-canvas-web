95667832a3e5aa3118402a4d7b4b0c89ca015629
Dillon <dpett122@gmail.com>
2019-06-19 12:05:43 -0400

    fix(errors): escape apostrophes

--- src/components/Interface/ImageDimensions/ImageSelectorAndForm/index.js
+++ src/components/Interface/ImageDimensions/ImageSelectorAndForm/index.js
@@ -60,7 +60,7 @@ export default class extends Component {
       <div>
         <h2>Image Dimensions</h2>
         <p>
-          <em>Images will maintain their original aspect ratio, so dimensions entered below that don't match an image's aspect ratio will create a border added to the image's height or width.</em>
+          <em>Images will maintain their original aspect ratio, so dimensions entered below that don&apos;t match an image&apos;s aspect ratio will create a border added to the image&apos;s height or width.</em>
         </p>
         <div
           style={{
